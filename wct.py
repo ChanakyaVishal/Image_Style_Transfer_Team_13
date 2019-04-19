11fad2fd1d2b92d227424571aadc710e5eea7145
ChanakyaVishal <chanakyavishal2@gmail.com>
2019-04-19 09:47:08 +0530

    Module to load models added

--- wct.py
+++ wct.py
@@ -6,3 +6,13 @@ from model import decoder1
 from model import encoder1
 import torch.nn as nn
 
+class WCT(nn.Module):
+    def __init__(self,args):
+        super(WCT, self).__init__()
+        # Load pre-trained network
+        vgg1 = load_lua(args.vgg1)
+        decoder1_torch = load_lua(args.decoder1)
+
+        self.e1 = encoder1(vgg1)
+        self.d1 = decoder1(decoder1_torch)
+
