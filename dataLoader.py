b42ae91d62459958f16514123b85c91c77b45cbc
Rachit Jain <jainrachit100@gmail.com>
2019-04-20 17:59:37 +0530

    Started adding dataset class

--- dataLoader.py
+++ dataLoader.py
@@ -11,3 +11,18 @@ from torch.autograd import Variable
 
 def isimg(filename):
     return any(filename.endswith(extension) for extension in [".png", ".jpg", ".jpeg"])
+
+class Dataset(data.Dataset):
+    def __init__(self,cp,sp,fineSize):
+        super(Dataset,self).__init__()
+        self.image_list = [x for x in os.listdir(cp) if isimg(x)]
+        self.fineSize = fineSize
+        self.stylePath = sp
+        self.contentPath = cp
+        self.prep = transforms.Compose([
+                    transforms.Scale(self.fineSize),
+                    transforms.ToTensor(),
+                    ])
+
+    def __len__(self):
+        return len(self.image_list)
