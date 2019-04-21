242a01524c15213005105c93b664c9f1694980c7
Rachit Jain <jainrachit100@gmail.com>
2019-04-21 17:22:42 +0530

    Added getitem function to get content and style image

--- dataLoader.py
+++ dataLoader.py
@@ -8,7 +8,6 @@ import torch.nn as nn
 import numpy as np
 from torch.autograd import Variable
 
-
 def isimg(filename):
     return any(filename.endswith(extension) for extension in [".png", ".jpg", ".jpeg"])
 
@@ -26,3 +25,22 @@ class Dataset(data.Dataset):
 
     def __len__(self):
         return len(self.image_list)
+
+    def __getitem__(self,index):
+        contentImgPath = os.path.join(self.contentPath,self.image_list[index])
+        styleImgPath = os.path.join(self.stylePath,self.image_list[index])
+        contentImg = Image.open(contentImgPath).convert('RGB')
+        styleImg = Image.open(styleImgPath).convert('RGB')
+
+        fz = self.fineSize
+        w,h = contentImg.size
+        if(fz == 0 and h != fz):
+            contentImg = contentImg.resize((int(w*newh/h),fz))
+            styleImg = styleImg.resize((int(w*newh/h),fz))
+        elif(fz != 0 and w>h and w != fz):
+            contentImg = contentImg.resize((fz,int(h*neww/w)))
+            styleImg = styleImg.resize((fz,int(h*neww/w)))
+
+        contentImg = transforms.ToTensor()(contentImg)
+        styleImg = transforms.ToTensor()(styleImg)
+        return contentImg.squeeze(0),styleImg.squeeze(0),self.image_list[index]
