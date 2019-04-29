c5dc108ac32405a6eeaccb74ea83e6b55d7bcd92
Rachit Jain <jainrachit100@gmail.com>
2019-04-29 21:26:09 +0530

    Added the support for multi styling in dataLoader

--- dataLoader.py
+++ dataLoader.py
@@ -8,39 +8,65 @@ import torch.nn as nn
 import numpy as np
 from torch.autograd import Variable
 
-def isimg(filename):
+def is_image_file(filename):
     return any(filename.endswith(extension) for extension in [".png", ".jpg", ".jpeg"])
 
+def default_loader(path):
+    return Image.open(path).convert('RGB')
+
 class Dataset(data.Dataset):
-    def __init__(self,cp,sp,fineSize):
+    def __init__(self,contentPath,stylePath,fineSize):
         super(Dataset,self).__init__()
-        self.image_list = [x for x in os.listdir(cp) if isimg(x)]
+        self.contentPath = contentPath
+        self.image_list = [x for x in os.listdir(contentPath) if is_image_file(x)]
+        self.stylePath = stylePath
         self.fineSize = fineSize
-        self.stylePath = sp
-        self.contentPath = cp
         self.prep = transforms.Compose([
-                    transforms.Scale(self.fineSize),
+                    transforms.Scale(fineSize),
                     transforms.ToTensor(),
                     ])
 
-    def __len__(self):
-        return len(self.image_list)
-
     def __getitem__(self,index):
         contentImgPath = os.path.join(self.contentPath,self.image_list[index])
-        styleImgPath = os.path.join(self.stylePath,self.image_list[index])
-        contentImg = Image.open(contentImgPath).convert('RGB')
-        styleImg = Image.open(styleImgPath).convert('RGB')
+        contentName = self.image_list[index][:-4]
+        styles = []
+        for style in os.listdir(stylePath):
+            if contentName in style:
+                styleImg = default_loader(os.path.join(self.stylePath,style))
+                styles.append(styleImg)
 
-        fz = self.fineSize
-        w,h = contentImg.size
-        if(fz == 0 and h != fz):
-            contentImg = contentImg.resize((int(w*newh/h),fz))
-            styleImg = styleImg.resize((int(w*newh/h),fz))
-        elif(fz != 0 and w>h and w != fz):
-            contentImg = contentImg.resize((fz,int(h*neww/w)))
-            styleImg = styleImg.resize((fz,int(h*neww/w)))
+        contentImg = default_loader(contentImgPath)
 
+
+
+        # resize
+        styles_rsz = []
+        for styleImg in styles:
+            if(self.fineSize != 0):
+                w,h = contentImg.size
+                if(w > h):
+                    if(w != self.fineSize):
+                        neww = self.fineSize
+                        newh = int(h*neww/w)
+                        contentImg = contentImg.resize((neww,newh))
+                        styleImg = styleImg.resize((neww,newh))
+                else:
+                    if(h != self.fineSize):
+                        newh = self.fineSize
+                        neww = int(w*newh/h)
+                        contentImg = contentImg.resize((neww,newh))
+                        styleImg = styleImg.resize((neww,newh))
+            styles_rsz.append(styleImg)
+
+
+        # Preprocess Images
         contentImg = transforms.ToTensor()(contentImg)
-        styleImg = transforms.ToTensor()(styleImg)
-        return contentImg.squeeze(0),styleImg.squeeze(0),self.image_list[index]
+        style_images = []
+        for styleImg in styles_rsz:
+            styleImg = transforms.ToTensor()(styleImg)
+            styleImg.squeeze(0)
+            style_images.append(styleImg)
+        return contentImg.squeeze(0),style_images,self.image_list[index]
+
+    def __len__(self):
+        return len(self.image_list)
